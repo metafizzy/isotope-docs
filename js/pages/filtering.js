06ebaf24fae107ef0825252193da42d0b7409085
David DeSandro <desandrocodes@gmail.com>
2014-03-19 20:41:58 -0400

    filtering code display

--- js/pages/filtering.js
+++ js/pages/filtering.js
@@ -20,22 +20,25 @@ ID.filtering = function() {
     });
 
     var filterFns = {
-      // show if number is greater than 50
-      numberGreaterThan50: function() {
-        var number = $(this).find('.number').text();
-        return parseInt( number, 10 ) > 50;
-      },
-      // show if name ends with -ium
-      ium: function() {
-        var name = $(this).find('.name').text();
-        return name.match( /ium$/ );
-      }
+// show if number is greater than 50
+numberGreaterThan50: function() {
+  var number = $(this).find('.number').text();
+  return parseInt( number, 10 ) > 50;
+},
+// show if name ends with -ium
+ium: function() {
+  var name = $(this).find('.name').text();
+  return name.match( /ium$/ );
+}
     };
+
+    var $codeDisplay = $('#filtering-demo .code-display code');
 
     $('#filtering-demo .button-group').on( 'click', 'button', function() {
       var filterValue = $(this).attr('data-filter');
       filterValue = filterFns[ filterValue ] || filterValue;
       $container.isotope({ filter: filterValue });
+      $codeDisplay.displayIsotopeCode( 'filter', filterValue );
     });
 
   })();
