5d9523573aee01267973553c5da73c5c0a9bb4cd
David DeSandro <desandrocodes@gmail.com>
2015-05-12 14:22:16 -0400

    filtering

---
+++ modules/combination-filters-demo/combination-filters-demo.js
@@ -0,0 +1,43 @@
+/**
+ * combination-filters-demo
+ */
+
+ID.modules['combination-filters-demo'] = function( elem ) {
+  'use strict';
+
+  var $demo = $( elem );
+
+  var $grid = $demo.find('.isotope-demo').isotope({
+    itemSelector: '.color-shape',
+    columnWidth: 80,
+    transitionDuration: '0.6s'
+  });
+
+  var $codeDisplay = $demo.find('.code-display code');
+
+  // store filter for each group
+  var filters = {};
+
+  $demo.on( 'click', '.button', function() {
+    var $this = $(this);
+    // get group key
+    var $buttonGroup = $this.parents('.button-group');
+    var filterGroup = $buttonGroup.attr('data-filter-group');
+    // set filter for group
+    filters[ filterGroup ] = $this.attr('data-filter');
+    // combine filters
+    var filterValue = concatValues( filters );
+    $grid.isotope({ filter: filterValue });
+    $codeDisplay.displayIsotopeCode( 'filter', filterValue );
+  });
+
+  // flatten object by concatting values
+  function concatValues( obj ) {
+    var value = '';
+    for ( var prop in obj ) {
+      value += obj[ prop ];
+    }
+    return value;
+  }
+
+};
