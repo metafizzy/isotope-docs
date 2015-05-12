05f084fe9afb790ad9041bf7f4df183323a8ae28
David DeSandro <desandrocodes@gmail.com>
2015-05-12 13:08:54 -0400

    filtering demo

--- js/pages/filtering.js
+++ js/pages/filtering.js
@@ -10,45 +10,6 @@ var ID = window.ID;
 
 ID.filtering = function() {
 
-  // demo at the top
-  ( function() {
-
-    var $container = $('#filtering-demo .isotope').isotope({
-      itemSelector: '.element-item',
-      layoutMode: 'fitRows',
-      transitionDuration: '0.6s'
-    });
-
-    var filterFns = {
-      // show if number is greater than 50
-      numberGreaterThan50: function() {
-        var number = $(this).find('.number').text();
-        return parseInt( number, 10 ) > 50;
-      },
-      // show if name ends with -ium
-      ium: function() {
-        var name = $(this).find('.name').text();
-        return name.match( /ium$/ );
-      }
-    };
-
-    var filterFnsDisplay = {
-      numberGreaterThan50: "function() {\n  var number = $(this).find('.number').text();\n  return parseInt( number, 10 ) > 50;\n}",
-      ium: 'function() {\n  var name = $(this).find(\'.name\').text();\n  return name.match( /ium$/ );\n}'
-    };
-
-    var $codeDisplay = $('#filtering-demo .code-display code');
-
-    $('#filtering-demo .button-group').on( 'click', 'button', function() {
-      var filterValue = $(this).attr('data-filter');
-      var isoFilterValue = filterFns[ filterValue ] || filterValue;
-      var displayFilterValue = filterFnsDisplay[ filterValue ] || filterValue;
-      $container.isotope({ filter: isoFilterValue });
-      $codeDisplay.displayIsotopeCode( 'filter', displayFilterValue );
-    });
-
-  })();
-
   // ----- combination filters ----- //
 
   ( function() {
---
+++ modules/filtering-demo/filtering-demo.js
@@ -0,0 +1,44 @@
+/**
+ * filtering-demo
+ */
+
+ID.modules['filtering-demo'] = function( elem ) {
+  'use strict';
+
+  var $demo = $( elem );
+
+  var $grid = $demo.find('.isotope-demo').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows',
+    transitionDuration: '0.6s'
+  });
+
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+      var number = $(this).find('.number').text();
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+
+  var filterFnsDisplay = {
+    numberGreaterThan50: "function() {\n  var number = $(this).find('.number').text();\n  return parseInt( number, 10 ) > 50;\n}",
+    ium: 'function() {\n  var name = $(this).find(\'.name\').text();\n  return name.match( /ium$/ );\n}'
+  };
+
+  var $codeDisplay = $demo.find('.code-display code');
+
+  $demo.find('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    var isoFilterValue = filterFns[ filterValue ] || filterValue;
+    var displayFilterValue = filterFnsDisplay[ filterValue ] || filterValue;
+    $grid.isotope({ filter: isoFilterValue });
+    $codeDisplay.displayIsotopeCode( 'filter', displayFilterValue );
+  });
+
+};
