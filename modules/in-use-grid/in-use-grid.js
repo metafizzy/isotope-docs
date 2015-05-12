e1cffe7b191a6971625374f740553e315d254125
David DeSandro <desandrocodes@gmail.com>
2015-05-12 11:09:02 -0400

    in-use-grid and primary-content to modules

---
+++ modules/in-use-grid/in-use-grid.js
@@ -0,0 +1,32 @@
+/**
+ * in-use-grid
+ */
+
+ID.modules['in-use-grid'] = function( elem ) {
+  'use strict';
+
+  var $grid = $(elem);
+  // hide items by default
+  $grid.find('.in-use-grid__item').hide();
+
+  $grid.isotope({
+    // select none
+    itemSelector: 'none',
+    masonry: {
+      columnWidth: '.grid-sizer',
+      gutter: '.gutter-sizer'
+    }
+  });
+
+  // now select items
+  $grid.isotope( 'option', { itemSelector: '.in-use-grid__item' } );
+
+  $grid.imagesLoaded().progress( function( imgLoad, image ) {
+    var $item = $( image.img ).parents( '.in-use-grid__item' );
+    // un-hide item
+    $item.show();
+    // masonry does its thing
+    $grid.isotope( 'appended', $item );
+  });
+
+};
