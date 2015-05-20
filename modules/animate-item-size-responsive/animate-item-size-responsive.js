0c5c88e4ad354753db8d33e675c383acd4c56609
David DeSandro <desandrocodes@gmail.com>
2015-05-20 10:19:06 -0400

    animate item size demos

---
+++ modules/animate-item-size-responsive/animate-item-size-responsive.js
@@ -0,0 +1,77 @@
+/**
+ * animate-item-size-responsive
+ */
+
+ID.modules['animate-item-size-responsive'] = function( elem ) {
+  'use strict';
+
+  var transitionProp = getStyleProperty('transition');
+  var transitionEndEvent = {
+    WebkitTransition: 'webkitTransitionEnd',
+    MozTransition: 'transitionend',
+    OTransition: 'otransitionend',
+    transition: 'transitionend'
+  }[ transitionProp ];
+
+  // ----- demo ----- //
+
+  var $demo = $( elem );
+
+  var $grid = $demo.find('.grid').isotope({
+    itemSelector: '.animate-item-size-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer'
+    }
+  });
+
+
+  $grid.on( 'click', '.animate-item-size-item__content', function() {
+    /*jshint unused: false */
+    var itemContent = this;
+    setItemContentPixelSize( itemContent );
+
+    var itemElem = itemContent.parentNode;
+    classie.toggleClass( itemElem, 'is-expanded' );
+
+    // force redraw
+    var redraw = itemContent.offsetWidth;
+    // renable default transition
+    itemContent.style[ transitionProp ] = '';
+
+    addTransitionListener( itemContent );
+    setItemContentTransitionSize( itemContent, itemElem );
+
+    $grid.isotope('layout');
+  });
+
+  function setItemContentPixelSize( itemContent ) {
+    var previousContentSize = getSize( itemContent );
+    // disable transition
+    itemContent.style[ transitionProp ] = 'none';
+    // set current size in pixels
+    itemContent.style.width = previousContentSize.width + 'px';
+    itemContent.style.height = previousContentSize.height + 'px';
+  }
+
+  function addTransitionListener( itemContent ) {
+    if ( !transitionProp ) {
+      return;
+    }
+    // reset 100%/100% sizing after transition end
+    var onTransitionEnd = function() {
+      itemContent.style.width = '';
+      itemContent.style.height = '';
+      itemContent.removeEventListener( transitionEndEvent, onTransitionEnd, false );
+    };
+    itemContent.addEventListener( transitionEndEvent, onTransitionEnd, false );
+  }
+
+  function setItemContentTransitionSize( itemContent, itemElem ) {
+    // set new size
+    var size = getSize( itemElem );
+    itemContent.style.width = size.width + 'px';
+    itemContent.style.height = size.height + 'px';
+  }
+
+};
---
+++ modules/animate-item-size/animate-item-size.js
@@ -0,0 +1,22 @@
+/**
+ * animate-item-size
+ */
+
+
+ID.modules['animate-item-size'] = function( elem ) {
+  'use strict';
+
+  var $demo = $( elem );
+
+  var $grid = $demo.find('.grid').isotope({
+    masonry: {
+      columnWidth: 60
+    }
+  });
+
+  $grid.on( 'click', '.animate-item-size-item', function() {
+    $(this).toggleClass('is-expanded');
+    $grid.isotope('layout');
+  });
+
+};
